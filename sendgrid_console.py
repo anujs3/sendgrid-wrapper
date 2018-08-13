ca33a5a1826f335e96e60006962e95550c302453
Anuj Shah <anuj.shah@sendgrid.com>
2018-08-12 23:10:30 -0600

    Refactoring Y/N inputs.

--- sendgrid_console.py
+++ sendgrid_console.py
@@ -5,11 +5,14 @@ import sendgrid_shared as shared
 def field_prompt(prompt_string):
     return prompt_string + ': '
 
-def yes_no(prompt_string):
+def boolean_formater(prompt_string):
     return prompt_string + ' [Y|N] '
 
-def validate_yesno(user_input):
+def validate_boolean(user_input):
     return re.match('^[Y|N]$', user_input.upper()) != None
+
+def boolean_question(prompt_string):
+    return handle_user_input(boolean_formater(prompt_string), validate_boolean).upper()
 
 def handle_user_input(prompt_string, match_condition=lambda input: input != ''):
     user_input = ''
@@ -24,22 +27,22 @@ def handle_user_input(prompt_string, match_condition=lambda input: input != ''):
 def main():
     print('Welcome to the Email Sender!')
     while True:
-        user_input = handle_user_input(yes_no('Do you want to send an email?'), validate_yesno).upper()
-        if user_input == 'Y':
+        send_boolean = boolean_question('Do you want to send an email?')
+        if send_boolean == 'Y':
             from_email = Email(handle_user_input(field_prompt('Sender'), shared.validate_email))
             to_email = Email(handle_user_input(field_prompt('Recipient'), shared.validate_email))
             subject = handle_user_input(field_prompt('Subject Line'))
             content = Content('text/plain', handle_user_input(field_prompt('Message')))
             mail = Mail(from_email, subject, to_email, content)
             while True:
-                attachment_prompt = handle_user_input(yes_no('Do you want to add an attachment?'), validate_yesno).upper()
-                if attachment_prompt == 'Y':
+                attachment_boolean = boolean_question('Do you want to add an attachment?')
+                if attachment_boolean == 'Y':
                     mail.add_attachment(shared.create_attachment(handle_user_input('Filepath of Attachment: ', shared.validate_file)))
                     print('Attachment was successfully attached to the email.')
-                elif attachment_prompt == 'N':
+                elif attachment_boolean == 'N':
                     break
             shared.send_mail(mail)
-        elif user_input == 'N':
+        elif send_boolean == 'N':
             print('Bye!')
             break
 
