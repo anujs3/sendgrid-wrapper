84308bf59fbd715cb4df4f064fbb40c8c8902829
Anuj Shah <anuj.shah@sendgrid.com>
2018-08-12 23:54:32 -0600

    Handler Helper Functions

--- sendgrid_cli.py
+++ sendgrid_cli.py
@@ -12,40 +12,52 @@ default_email = os.environ.get('DEFAULT_EMAIL')
 @click.option('--recipient', default=default_email, help='The Email Address for Receiving Mail')
 @click.option('--subject', default='Test', help='The Subject Line')
 @click.option('--message', default='This is a test.', help='Body of the Email')
-@click.option('--attachmentlist', help='Text File with File Paths of Attachments')
+@click.option('--attachments', help='Text File with File Paths of Attachments')
 @click.option('--schedule', help='Date/Time to Send Email')
 @click.option('--cc', help='Email Address for Carbon Copy')
 @click.option('--bcc', help='Email Address for Blocked Carbon Copy')
-def main(sender,recipient,subject,message, attachmentlist, schedule, cc, bcc):
+def main(sender,recipient,subject,message, attachments, schedule, cc, bcc):
     if shared.validate_email(sender) and shared.validate_email(recipient):
         from_email = Email(sender)
         to_email = Email(recipient)
         content = Content('text/plain', message)
         mail = Mail(from_email, subject, to_email, content)
-        if attachmentlist != None:
-            with open(attachmentlist) as file:
-                for line in file.readlines():
-                    line = line.strip()
-                    mail.add_attachment(shared.create_attachment(line))
-        if schedule != None:
-            timestamp = int(time.mktime(datetime.datetime.strptime(schedule, '%m-%d-%Y %H:%M:%S').timetuple()))
-            if shared.validate_scheduled_date(timestamp):
-                mail.send_at = timestamp
-            else:
-                error_handler('The scheduled date is not valid.')
-        if cc != None:
-            if shared.validate_email(cc):
-                mail.personalizations[0].add_cc(Email(cc))
-            else:
-                error_handler('The CC email is invalid.')
-        if bcc != None:
-            if shared.validate_email(bcc):
-                mail.personalizations[0].add_bcc(Email(bcc))
-            else:
-                error_handler('The BCC email is invalid.')
+        attachment_handler(attachments, mail)
+        schedule_handler(schedule, mail)
+        cc_handler(cc, mail)
+        bcc_handler(bcc, mail)
         shared.send_mail(mail)
     else:
         error_handler('One or more emails are invalid.')
+
+def attachment_handler(attachments, mail):
+    if attachments != None:
+        with open(attachments) as file:
+            for line in file.readlines():
+                line = line.strip()
+                mail.add_attachment(shared.create_attachment(line))
+
+def schedule_handler(schedule, mail):
+    if schedule != None:
+        timestamp = int(time.mktime(datetime.datetime.strptime(schedule, '%m-%d-%Y %H:%M:%S').timetuple()))
+        if shared.validate_scheduled_date(timestamp):
+            mail.send_at = timestamp
+        else:
+            error_handler('The scheduled date is not valid.')
+
+def cc_handler(cc, mail):
+    if cc != None:
+        if shared.validate_email(cc):
+            mail.personalizations[0].add_cc(Email(cc))
+        else:
+            error_handler('The CC email is invalid.')
+
+def bcc_handler(bcc, mail):
+    if bcc != None:
+        if shared.validate_email(bcc):
+            mail.personalizations[0].add_bcc(Email(bcc))
+        else:
+            error_handler('The BCC email is invalid.')
 
 def error_handler(error_message, exit_code=1):
     click.echo('Error: ' + error_message)
